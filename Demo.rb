a08685b46cf451f3a3f780e5c7b8ad9202979421
Jenny Blessing <122192009+jennyxblessing@users.noreply.github.com>
2023-01-22 20:38:25 -0800

    Update Demo.rb

--- Demo.rb
+++ Demo.rb
@@ -80,3 +80,59 @@ describe "view cart" do
 	end
 end
 
+describe "Enter info/cancel" do
+	it "Fills out First/Last/Zip code fields and cancels" do
+ 				
+		wait.until {driver.find_element(id: 'checkout_info_container').displayed?}
+		expect(driver.find_element(id: 'cancel').text).to eql("CANCEL")
+		driver.find_element(id: 'first-name').send_keys(firstName)
+		driver.find_element(id: 'last-name').send_keys(lastName)
+		driver.find_element(id: 'postal-code').send_keys(zip)
+		driver.find_element(id: 'cancel').click
+
+		sleep 1
+	end
+end
+
+
+describe "Leave info blank error check: first name" do
+	it "Attempts to CONTINUE w/o adding info, checks for errors" do
+ 		
+ 		driver.find_element(id: 'checkout').click		
+		wait.until {driver.find_element(id: 'checkout_info_container').displayed?}
+		driver.find_element(id: 'continue').click
+		wait.until {driver.find_element(:xpath, '/html/body/div/div/div/div[2]/div/form/div[1]/div[4]').displayed?}
+		expect(driver.find_element(:xpath, '/html/body/div/div/div/div[2]/div/form/div[1]/div[4]').text).to eql("Error: First Name is required")
+		driver.find_element(id: 'first-name').send_keys(firstName)
+		driver.find_element(id: 'continue').click
+
+		sleep 1
+	end
+end
+
+describe "Leave info blank error check: last name" do
+	it "Attempts to CONTINUE w/o adding info, checks for errors" do
+ 				
+		wait.until {driver.find_element(:xpath, '/html/body/div/div/div/div[2]/div/form/div[1]/div[4]').displayed?}
+		expect(driver.find_element(:xpath, '/html/body/div/div/div/div[2]/div/form/div[1]/div[4]').text).to eql("Error: Last Name is required")
+		driver.find_element(id: 'last-name').send_keys(lastName)
+		driver.find_element(id: 'continue').click
+
+		sleep 1
+	end
+end
+
+
+describe "Leave info blank error check: postal code" do
+	it "Attempts to CONTINUE w/o adding info, checks for errors" do
+ 				
+		wait.until {driver.find_element(:xpath, '/html/body/div/div/div/div[2]/div/form/div[1]/div[4]').displayed?}
+		expect(driver.find_element(:xpath, '/html/body/div/div/div/div[2]/div/form/div[1]/div[4]').text).to eql("Error: Postal Code is required")
+		driver.find_element(id: 'postal-code').send_keys(zip)
+		driver.find_element(id: 'continue').click
+
+		sleep 1
+	end
+end
+
+
